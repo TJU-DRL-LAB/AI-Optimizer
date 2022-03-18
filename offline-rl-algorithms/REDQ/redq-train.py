da46fb5f7eb68cbb2968aed56848de7083ea693f
马亿 <18222860970@163.com>
2022-03-18 16:52:44 +0800

    Add files via upload

---
+++ offline-rl-algorithms/REDQ/redq-train.py
@@ -0,0 +1,49 @@
+import argparse
+import gym
+import tjuOfflineRL
+import torch
+
+
+def main():
+
+    torch.set_num_threads(1)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--gpu', type=bool, default=False)
+    args = parser.parse_args()
+
+    env = gym.make(args.env)
+    eval_env = gym.make(args.env)
+
+    # fix seed
+    tjuOfflineRL.seed(args.seed)
+    env.seed(args.seed)
+    eval_env.seed(args.seed)
+
+    # load algorithm
+    redq = tjuOfflineRL.algos.REDQ(batch_size=256,
+                                   actor_learning_rate=3e-4,
+                                   critic_learning_rate=3e-4,
+                                   temp_learning_rate=3e-4,
+                                   use_gpu=args.gpu)
+
+    # replay buffer for experience replay
+    buffer = tjuOfflineRL.online.buffers.ReplayBuffer(maxlen=1000000, env=env)
+
+    # start training
+    redq.fit_online_redq(env,
+                         buffer,
+                         eval_env=eval_env,
+                         n_steps=1000000,
+                         n_steps_per_epoch=1000,
+                         update_interval=1,
+                         update_start_step=1000,
+                         save_interval=100,
+                         tensorboard_dir='runs',
+                         experiment_name=f"REDQ_online_{args.env}_medium-replay_{args.seed}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ offline-rl-algorithms/REDQ/redq.py
@@ -0,0 +1,260 @@
+from typing import Any, Dict, Optional, Sequence
+
+from ..argument_utility import (
+    ActionScalerArg,
+    EncoderArg,
+    QFuncArg,
+    RewardScalerArg,
+    ScalerArg,
+    UseGPUArg,
+    check_encoder,
+    check_q_func,
+    check_use_gpu,
+)
+from ..constants import IMPL_NOT_INITIALIZED_ERROR, ActionSpace
+from ..dataset import TransitionMiniBatch
+from ..gpu import Device
+from ..models.encoders import EncoderFactory
+from ..models.optimizers import AdamFactory, OptimizerFactory
+from ..models.q_functions import QFunctionFactory
+from .base import AlgoBase
+from .torch.redq_impl import DiscreteREDQImpl, REDQImpl
+
+
+class REDQ(AlgoBase):
+
+    _actor_learning_rate: float
+    _critic_learning_rate: float
+    _temp_learning_rate: float
+    _actor_optim_factory: OptimizerFactory
+    _critic_optim_factory: OptimizerFactory
+    _temp_optim_factory: OptimizerFactory
+    _actor_encoder_factory: EncoderFactory
+    _critic_encoder_factory: EncoderFactory
+    _q_func_factory: QFunctionFactory
+    _tau: float
+    _n_critics: int
+    _initial_temperature: float
+    _use_gpu: Optional[Device]
+    _impl: Optional[REDQImpl]
+
+    def __init__(
+        self,
+        *,
+        actor_learning_rate: float = 3e-4,
+        critic_learning_rate: float = 3e-4,
+        temp_learning_rate: float = 3e-4,
+        actor_optim_factory: OptimizerFactory = AdamFactory(),
+        critic_optim_factory: OptimizerFactory = AdamFactory(),
+        temp_optim_factory: OptimizerFactory = AdamFactory(),
+        actor_encoder_factory: EncoderArg = "default",
+        critic_encoder_factory: EncoderArg = "default",
+        q_func_factory: QFuncArg = "mean",
+        batch_size: int = 256,
+        n_frames: int = 1,
+        n_steps: int = 1,
+        gamma: float = 0.99,
+        tau: float = 0.005,
+        n_critics: int = 2,
+        initial_temperature: float = 1.0,
+        use_gpu: UseGPUArg = False,
+        scaler: ScalerArg = None,
+        action_scaler: ActionScalerArg = None,
+        reward_scaler: RewardScalerArg = None,
+        impl: Optional[REDQImpl] = None,
+        **kwargs: Any
+    ):
+        super().__init__(
+            batch_size=batch_size,
+            n_frames=n_frames,
+            n_steps=n_steps,
+            gamma=gamma,
+            scaler=scaler,
+            action_scaler=action_scaler,
+            reward_scaler=reward_scaler,
+            kwargs=kwargs,
+        )
+        self._actor_learning_rate = actor_learning_rate
+        self._critic_learning_rate = critic_learning_rate
+        self._temp_learning_rate = temp_learning_rate
+        self._actor_optim_factory = actor_optim_factory
+        self._critic_optim_factory = critic_optim_factory
+        self._temp_optim_factory = temp_optim_factory
+        self._actor_encoder_factory = check_encoder(actor_encoder_factory)
+        self._critic_encoder_factory = check_encoder(critic_encoder_factory)
+        self._q_func_factory = check_q_func(q_func_factory)
+        self._tau = tau
+        self._n_critics = n_critics
+        self._initial_temperature = initial_temperature
+        self._use_gpu = check_use_gpu(use_gpu)
+        self._impl = impl
+
+    def _create_impl(
+        self, observation_shape: Sequence[int], action_size: int
+    ) -> None:
+        self._impl = REDQImpl(
+            observation_shape=observation_shape,
+            action_size=action_size,
+            actor_learning_rate=self._actor_learning_rate,
+            critic_learning_rate=self._critic_learning_rate,
+            temp_learning_rate=self._temp_learning_rate,
+            actor_optim_factory=self._actor_optim_factory,
+            critic_optim_factory=self._critic_optim_factory,
+            temp_optim_factory=self._temp_optim_factory,
+            actor_encoder_factory=self._actor_encoder_factory,
+            critic_encoder_factory=self._critic_encoder_factory,
+            q_func_factory=self._q_func_factory,
+            gamma=self._gamma,
+            tau=self._tau,
+            n_critics=self._n_critics,
+            initial_temperature=self._initial_temperature,
+            use_gpu=self._use_gpu,
+            scaler=self._scaler,
+            action_scaler=self._action_scaler,
+            reward_scaler=self._reward_scaler,
+        )
+        self._impl.build()
+
+    def _update_redq(self, batch: TransitionMiniBatch, i_update: int) -> Dict[str, float]:
+        assert self._impl is not None, IMPL_NOT_INITIALIZED_ERROR
+
+        metrics = {}
+
+        critic_loss = self._impl.update_critic(batch)
+        metrics.update({"critic_loss": critic_loss})
+
+        self._impl.update_critic_target()
+
+        if (i_update + 1) % 20 == 0:
+            # lagrangian parameter update for SAC temperature
+            if self._temp_learning_rate > 0:
+                temp_loss, temp = self._impl.update_temp(batch)
+                metrics.update({"temp_loss": temp_loss, "temp": temp})
+
+            actor_loss = self._impl.update_actor(batch)
+            metrics.update({"actor_loss": actor_loss})
+
+            self._impl.update_actor_target()
+
+        return metrics
+
+    def get_action_type(self) -> ActionSpace:
+        return ActionSpace.CONTINUOUS
+
+
+class DiscreteSAC(AlgoBase):
+
+    _actor_learning_rate: float
+    _critic_learning_rate: float
+    _temp_learning_rate: float
+    _actor_optim_factory: OptimizerFactory
+    _critic_optim_factory: OptimizerFactory
+    _temp_optim_factory: OptimizerFactory
+    _actor_encoder_factory: EncoderFactory
+    _critic_encoder_factory: EncoderFactory
+    _q_func_factory: QFunctionFactory
+    _n_critics: int
+    _initial_temperature: float
+    _target_update_interval: int
+    _use_gpu: Optional[Device]
+    _impl: Optional[DiscreteREDQImpl]
+
+    def __init__(
+        self,
+        *,
+        actor_learning_rate: float = 3e-4,
+        critic_learning_rate: float = 3e-4,
+        temp_learning_rate: float = 3e-4,
+        actor_optim_factory: OptimizerFactory = AdamFactory(eps=1e-4),
+        critic_optim_factory: OptimizerFactory = AdamFactory(eps=1e-4),
+        temp_optim_factory: OptimizerFactory = AdamFactory(eps=1e-4),
+        actor_encoder_factory: EncoderArg = "default",
+        critic_encoder_factory: EncoderArg = "default",
+        q_func_factory: QFuncArg = "mean",
+        batch_size: int = 64,
+        n_frames: int = 1,
+        n_steps: int = 1,
+        gamma: float = 0.99,
+        n_critics: int = 2,
+        initial_temperature: float = 1.0,
+        target_update_interval: int = 8000,
+        use_gpu: UseGPUArg = False,
+        scaler: ScalerArg = None,
+        reward_scaler: RewardScalerArg = None,
+        impl: Optional[DiscreteREDQImpl] = None,
+        **kwargs: Any
+    ):
+        super().__init__(
+            batch_size=batch_size,
+            n_frames=n_frames,
+            n_steps=n_steps,
+            gamma=gamma,
+            scaler=scaler,
+            action_scaler=None,
+            reward_scaler=reward_scaler,
+            kwargs=kwargs,
+        )
+        self._actor_learning_rate = actor_learning_rate
+        self._critic_learning_rate = critic_learning_rate
+        self._temp_learning_rate = temp_learning_rate
+        self._actor_optim_factory = actor_optim_factory
+        self._critic_optim_factory = critic_optim_factory
+        self._temp_optim_factory = temp_optim_factory
+        self._actor_encoder_factory = check_encoder(actor_encoder_factory)
+        self._critic_encoder_factory = check_encoder(critic_encoder_factory)
+        self._q_func_factory = check_q_func(q_func_factory)
+        self._n_critics = n_critics
+        self._initial_temperature = initial_temperature
+        self._target_update_interval = target_update_interval
+        self._use_gpu = check_use_gpu(use_gpu)
+        self._impl = impl
+
+    def _create_impl(
+        self, observation_shape: Sequence[int], action_size: int
+    ) -> None:
+        self._impl = DiscreteREDQImpl(
+            observation_shape=observation_shape,
+            action_size=action_size,
+            actor_learning_rate=self._actor_learning_rate,
+            critic_learning_rate=self._critic_learning_rate,
+            temp_learning_rate=self._temp_learning_rate,
+            actor_optim_factory=self._actor_optim_factory,
+            critic_optim_factory=self._critic_optim_factory,
+            temp_optim_factory=self._temp_optim_factory,
+            actor_encoder_factory=self._actor_encoder_factory,
+            critic_encoder_factory=self._critic_encoder_factory,
+            q_func_factory=self._q_func_factory,
+            gamma=self._gamma,
+            n_critics=self._n_critics,
+            initial_temperature=self._initial_temperature,
+            use_gpu=self._use_gpu,
+            scaler=self._scaler,
+            reward_scaler=self._reward_scaler,
+        )
+        self._impl.build()
+
+    def _update_redq(self, batch: TransitionMiniBatch, i_update: int) -> Dict[str, float]:
+        assert self._impl is not None, IMPL_NOT_INITIALIZED_ERROR
+
+        metrics = {}
+
+        critic_loss = self._impl.update_critic(batch)
+        metrics.update({"critic_loss": critic_loss})
+
+        self._impl.update_critic_target()
+
+        if (i_update + 1) % 20 == 0:
+            # lagrangian parameter update for SAC temperature
+            if self._temp_learning_rate > 0:
+                temp_loss, temp = self._impl.update_temp(batch)
+                metrics.update({"temp_loss": temp_loss, "temp": temp})
+
+            actor_loss = self._impl.update_actor(batch)
+            metrics.update({"actor_loss": actor_loss})
+
+            self._impl.update_actor_target()
+
+        return metrics
+
+    def get_action_type(self) -> ActionSpace:
+        return ActionSpace.DISCRETE
